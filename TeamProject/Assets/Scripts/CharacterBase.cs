54acd4aab3ca423c13443d17275a409c32078f17
Elce08 <yeonhahahahaha@gmail.com>
2023-06-05 19:28:38 +0900

    20230605

---
+++ TeamProject/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+    //데이지 애니매이션
+    GameObject Hit;
+    //최종 데미지
+    float Damage;
+    //기본 스탯
+    float Strike;
+    float Intelligent;
+    float Agility;
+    float Defence;
+    float Anti;
+    float hp;
+    float mp;
+    //최대 마나체력
+    public float MaxHp = 100f;
+    public float MaxMp = 100f;
+    //스탯 배수 설정
+    public float StrikeTime;
+    public float IntelligentTime;
+    public float DefenceTime;
+    public float AntiTime;
+    //치명타 배수
+    public float Critical = 2f;
+    //임시 데미지 이펙트 시간
+    public float AnimationTime = 1f;
+    //마나 체력 받기
+    public float HP
+    {
+        get => MaxHp;
+        protected set
+        {
+            if(hp != value)
+            {
+                hp = value;
+                if(hp < 0)
+                {
+                    hp = 0;
+                    Die();
+                }
+            }
+        }
+    }
+    public float MP
+    {
+        get => MaxMp;
+        protected set
+        {
+            if(mp != value)
+            {
+                mp = value;
+            }
+        }
+    }
+    
+    /// <summary>
+    /// 캐릭터 스탯
+    /// </summary>
+    protected virtual void Awake()
+    {
+        Strike = 1f;
+        Intelligent = 50f;
+        Agility = 1f;
+        Defence = 1f * DefenceTime;
+        Anti = 1f * AntiTime;
+        Hit = transform.GetChild(0).gameObject;
+        Hit.SetActive(false);
+    }
+    
+    /// <summary>
+    /// 주는 데미지
+    /// </summary>
+    /// <returns>주는 데미지 리턴</returns>
+    protected virtual float Attack()
+    {
+        if (Random.Range(0, 100) < Agility)
+        {
+            Damage = (Strike * StrikeTime + Intelligent * IntelligentTime) * Critical;
+        }
+        else Damage = (Strike * StrikeTime + Intelligent * IntelligentTime);
+        return Damage;
+    }
+
+    /// <summary>
+    /// 받는 데미지
+    /// </summary>
+    /// <param name="getDamage">데미지</param>
+    /// <param name="DamageSort">받는 데미지 종류</param>
+    protected virtual void getDemage(float getDamage, int DamageSort)
+    {
+        if(DamageSort == 0) HP -= getDamage / Defence;
+        else if(DamageSort == 1) HP -= getDamage / Anti;
+        StartCoroutine(hit());
+    }
+
+    /// <summary>
+    /// 으앙주금
+    /// </summary>
+    protected virtual void Die()
+    {
+        Debug.Log("Die");
+    }
+
+    /// <summary>
+    /// 데이지 애니메이션 실행
+    /// </summary>
+    /// <returns>AnimationTime시간 만큼 실행</returns>
+    IEnumerator hit()
+    {
+        Hit.SetActive(true);
+        yield return AnimationTime;
+        Hit.SetActive(false);
+    }
+}
